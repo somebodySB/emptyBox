ee982d5de85f61f73c637f92e412c4e2aa1486ff
somebodySB <936567713@qq.com>
2018-05-24 22:08:34 +0800

    5.24 22:08

--- public/javascripts/showAnswer.js
+++ public/javascripts/showAnswer.js
@@ -3,6 +3,7 @@
   var firstClassList = [];
   var secondClassList = [];
   var thirdClassList = [];
+
   for(var i=0;i<score.length;i++){
     switch (score[i]) {
       case 100:
@@ -19,21 +20,75 @@
     }
   }
   var sumPict = 0;
+  var virusNames = [
+    "R3综合征",
+    "高斯模糊",
+    "友抑",
+    "灰粉",
+    "九号综合征",
+    "水晶谎言",
+    "冻暄症",
+    "无声流感",
+    "倦投症",
+    "定位症"
+  ]
+  function selectReport(){
+    var report = document.getElementById("report");
+    var num = Math.ceil(Math.random()*4);
+    report.src="/images/answer/report"+num+".png";
+  }
+  selectReport();
+
+  var greetings = [
+    "早睡早起，多喝热水",
+    "注重发量，救救孩子",
+    "戒骄戒躁，适度佛系",
+    "嗯，没毛病",
+    "注意闪退，及时保存"
+  ]
+  function selectGreeting(){
+    var greeting = document.getElementById("greeting");
+    greeting.innerHTML = greetings[Math.floor(Math.random()*greetings.length)];
+  }
+  selectGreeting();
+  var myDate = new Date();
+  function showTime(){
+    var time = document.getElementById("date");
+
+    time.innerHTML=myDate.getFullYear()+"."+(myDate.getMonth()+1)+"."+myDate.getDate();
+  }
+
+  showTime();
   for(var i=0;i<firstClassList.length;i++){
     if(sumPict>=3){
       break;
     }
-    var img = document.getElementById("Img"+(sumPict+1));
-    img.src = "/images/"+firstClassList[i]+".png";
-    sumPict++;
+    if(Math.random()*100<=70){
+      var img = document.getElementById("virus"+(sumPict+1));
+      img.src = "/images/answer/"+firstClassList[i]+".png";
+
+      var virusName = document.getElementById("virusName"+(sumPict+1));
+      virusName.innerHTML = virusNames[firstClassList[i]-1];
+
+      var ratio = document.getElementById("ratio"+(sumPict+1));
+      ratio.innerHTML = Math.ceil(Math.random()*25+65)+"%";
+      sumPict++;
+    }
+
   }
   for(var i=0;i<secondClassList.length;i++){
     if(sumPict>=3){
       break;
     }
     if(Math.random()*100<=30){
-      var img = document.getElementById("Img"+(sumPict+1));
-      img.src = "/images/"+secondClassList[i]+".png";
+      var img = document.getElementById("virus"+(sumPict+1));
+      img.src = "/images/answer/"+secondClassList[i]+".png";
+
+      var virusName = document.getElementById("virusName"+(sumPict+1));
+      virusName.innerHTML = virusNames[secondClassList[i]-1];
+
+      var ratio = document.getElementById("ratio"+(sumPict+1));
+      ratio.innerHTML = Math.ceil(Math.random()*15+40)+"%";
       sumPict++;
     }
   }
@@ -42,32 +97,45 @@
       break;
     }
     if(Math.random()*100<=10){
-      var img = document.getElementById("Img"+(sumPict+1));
-      img.src = "/images/"+thirdClassList[i]+".png";
+      var img = document.getElementById("virus"+(sumPict+1));
+      img.src = "/images/answer/"+thirdClassList[i]+".png";
+
+      var virusName = document.getElementById("virusName"+(sumPict+1));
+      virusName.innerHTML = virusNames[thirdClassList[i]-1];
+
+      var ratio = document.getElementById("ratio"+(sumPict+1));
+      ratio.innerHTML = Math.ceil(Math.random()*25+15)+"%";
       sumPict++;
     }
 
   }
-  var myCanvas = document.getElementById("mCan");
-  html2canvas(document.querySelector("#test")).then(canvas => {
+
+  for(sumPict;sumPict<3;sumPict++){
+    var randomId = Math.ceil(Math.random()*3+7);
+    var img = document.getElementById("virus"+(sumPict+1));
+    img.src = "/images/answer/"+randomId+".png";
+
+    var virusName = document.getElementById("virusName"+(sumPict+1));
+    virusName.innerHTML = virusNames[randomId-1];
+
+    var ratio = document.getElementById("ratio"+(sumPict+1));
+    ratio.innerHTML = Math.ceil(Math.random()*25+15)+"%";
+  }
+
+  var inviteDate = document.getElementById("inviteDate");
+  inviteDate.innerHTML = (myDate.getMonth()+1)+" / "+myDate.getDate()+" - 6 / 10";
+
+  var myCanvas = document.getElementById("myCanvas");
+  html2canvas(document.querySelector("#container"),{
+    backgroundColor:null,
+  }).then(canvas => {
+
     document.body.appendChild(canvas);
     canvas.style.display = "none";
-    var myimg = Canvas2Image.convertToJPEG(canvas,canvas.width,canvas.height);
-    myimg.style.width = "100%";
+    var myimg = Canvas2Image.convertToPNG(canvas,canvas.width,canvas.height);
+    myimg.style.width = "8.73rem";
+    myimg.style.position = "absolute";
+    myimg.style.left = ".65rem";
+    myimg.style.top = ".93rem";
     document.body.appendChild(myimg);
   });
-  // Canvas2Image.saveAsJPEG(myCanvas,640,320);
-  // document.body.appendChild(myimg);
-  // var myCanvas = document.getElementsByTagName("canvas");
-  // while(true){
-  //   var img = document.createElement("image");
-  //   img.src = "./"+
-  // }
-
-  // function downImg(){
-  //   var myimg = Canvas2Image.convertToJPEG(myCanvas,myCanvas.width,myCanvas.height);
-  //   document.body.appendChild(myimg);
-  //   myimg.style.width = "100%";
-  //   var button = document.getElementById("getImg");
-  //   button.style.display = "none";
-  // }
--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,82 @@
 var express = require('express');
 var bodyParser = require("body-parser");
-//var multer = require("multer");
+var youziku =require("youziku");
+var youzikuClient = new youziku.youzikuClient("54d2ff5e56a090675039254ec6b1d448"); //apikey
+var file = require("fs");
 var router = function(app){
   app.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
   });
-  app.get('/test', function(req, res, next) {
-    res.render('test', { title: 'Express' });
+  app.get('/main', function(req, res, next) {
+    res.render('main', { title: 'Express' });
+
   });
   app.post('/answer',function(req,res,next){
     var score = [];
     for(var i=0;i<req.body.length;i++){
       score.push(req.body[i]);
     }
+    var TestAnswer="访问者 "+req.body.name+" 测试结果为： ";
+    for (var i = 0; i < score.length; i++) {
+      TestAnswer = TestAnswer + score[i] + " ";
+    }
+    file.writeFile('./testeeAnswer.txt', TestAnswer+"\n", { 'flag': 'a' }, function(err) {
+      if (err) {
+          throw err;
+      }
+
+      console.log('Saved.');
+
+      // 写入成功后读取测试
+      file.readFile('./testeeAnswer.txt', 'utf-8', function(err, data) {
+          if (err) {
+              throw err;
+          }
+          console.log(data);
+      });
+    });
     res.render('answer', { title: '我的病历', score: score, name: req.body.name});
-    console.log(req.body.length);
-    console.log(req.body.name);
   });
+
+
+
   app.post('/question',function(req,res,next){
-    res.render('question',{title:'诊断中……', name: req.body.name})
+    res.render('question',{title:'诊断中……', name: req.body.name});
+    text = "早睡早起，多喝热水"+
+        "注重发量，救救孩子"+
+        "戒骄戒躁，适度佛系"+
+        "嗯，没毛病"+
+        "注意闪退，及时保存"+
+        "0123456789%/-"+
+        "江南大学数字媒体学院"+
+        "R3综合征"+
+        "高斯模糊"+
+        "友抑"+
+        "灰粉"+
+        "九号综合征"+
+        "水晶谎言"+
+        "冻暄症"+
+        "无声流感"+
+        "倦投症"+
+        "定位症"+
+        "你已经足够了解我们你准备好了解自己了吗？"+
+        "请输入你的代号"+
+        "ok"+
+        req.body.name;
+
+    var cdata = {
+        Datas: []
+    };
+
+    cdata.Datas.push({ AccessKey: 'fc5782dd8f4a4146a160b43787c467f4',
+                        Content:    text,
+                        Url: 'emptyBox/font/myFont' });
+    youzikuClient.createBatchWoffWebFontAsync(cdata, function (result) {
+    console.log(result.Code);
+    console.log(result.ErrorMessage);
+    console.log(text);
   })
+    });
   app.get('/answer', function(req, res, next) {
     res.render('index', { title: 'Express' });
   });
