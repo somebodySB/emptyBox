2362928bb54102071d226fc47dbd70e635c7a96e
somebodySB <936567713@qq.com>
2018-05-25 10:31:49 +0800

    5.25 10:31

--- public/javascripts/showAnswer.js
+++ public/javascripts/showAnswer.js
@@ -28,9 +28,9 @@
     "九号综合征",
     "水晶谎言",
     "冻暄症",
-    "无声流感",
+    "定位症",
     "倦投症",
-    "定位症"
+    "无声流感"
   ]
   function selectReport(){
     var report = document.getElementById("report");
@@ -111,31 +111,31 @@
   }
 
   for(sumPict;sumPict<3;sumPict++){
-    var randomId = Math.ceil(Math.random()*3+7);
+    var Id = 8+sumPict;
     var img = document.getElementById("virus"+(sumPict+1));
-    img.src = "/images/answer/"+randomId+".png";
+    img.src = "/images/answer/"+Id+".png";
 
     var virusName = document.getElementById("virusName"+(sumPict+1));
-    virusName.innerHTML = virusNames[randomId-1];
+    virusName.innerHTML = virusNames[Id-1];
 
     var ratio = document.getElementById("ratio"+(sumPict+1));
     ratio.innerHTML = Math.ceil(Math.random()*25+15)+"%";
   }
 
-  var inviteDate = document.getElementById("inviteDate");
-  inviteDate.innerHTML = (myDate.getMonth()+1)+" / "+myDate.getDate()+" - 6 / 10";
-
   var myCanvas = document.getElementById("myCanvas");
-  html2canvas(document.querySelector("#container"),{
-    backgroundColor:null,
-  }).then(canvas => {
 
-    document.body.appendChild(canvas);
-    canvas.style.display = "none";
-    var myimg = Canvas2Image.convertToPNG(canvas,canvas.width,canvas.height);
-    myimg.style.width = "8.73rem";
-    myimg.style.position = "absolute";
-    myimg.style.left = ".65rem";
-    myimg.style.top = ".93rem";
-    document.body.appendChild(myimg);
-  });
+  setTimeout(function () {
+    html2canvas(document.querySelector("#container"),{
+      backgroundColor:null,
+    }).then(canvas => {
+
+      document.body.appendChild(canvas);
+      canvas.style.display = "none";
+      var myimg = Canvas2Image.convertToPNG(canvas,canvas.width,canvas.height);
+      myimg.style.width = "8.73rem";
+      myimg.style.position = "absolute";
+      myimg.style.left = ".65rem";
+      myimg.style.top = ".93rem";
+      document.body.appendChild(myimg);
+    });
+  }, 1000);
